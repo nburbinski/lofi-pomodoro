ff74d6e7dd61999ce6fb096ce4087a3f7df5e2d8
Nicolas Burbinski <nburbinski@gmail.com>
2022-05-21 11:44:54 -0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
